e486b0d2094cf061cea030134a9607eca091cff9
jogi9928 <jogi9928@colorado.edu>
2023-04-11 11:18:05 -0600

    finishing

--- wa/wa11/wa11.js
+++ wa/wa11/wa11.js
@@ -1,10 +1,11 @@
 const btn = document.querySelector('#js-new-quote');
 btn.addEventListener('click', getQuote);
 const endpoint = 'https://meowfacts.herokuapp.com';
+const End = 'https://meowfacts.herokuapp.com/?count=2';
 
 let answerQ = '';
 const answerButton = document.querySelector('#js-tweet');
-answerButton.addEventListener('click', answerQuote);
+answerButton.addEventListener('click', get3Quote);
 async function getQuote()
 {
     try
@@ -18,7 +19,28 @@ async function getQuote()
         console.log(json);
         displayQuote(json.data);
         const answerText = document.querySelector('#js-answer-text');
-        
+        displayQuote1('');
+    }
+    catch(err)
+    {
+        console.log(err);
+        alert('Failed to fetch new quote');
+    }
+}
+async function get3Quote()
+{
+    try
+    {
+        const response = await fetch(End);
+        if(!response.ok)
+        {
+            throw Error(response.statusText);
+        }
+        const json = await response.json();
+        console.log(json);
+        displayQuote(json.data[0]);
+        displayQuote1(json.data[1]);
+        const answerTxt = document.querySelector('#js-answer-text');
     }
     catch(err)
     {
@@ -32,16 +54,14 @@ function displayQuote(quote)
     const quoteText = document.querySelector('#js-quote-text');
     quoteText.textContent = quote;
 }
+function displayQuote1(quote)
+{
+    const quoteText = document.querySelector('#js-answer-text');
+    quoteText.textContent = quote;
+}
+
 
 getQuote();
-
-
-function answerQuote()
-{
-    const answerText = document.querySelector('#js-answer-text');
-    answerText.textContent = answerQ;
-    console.log(answerQ);
-}
 
 
 
