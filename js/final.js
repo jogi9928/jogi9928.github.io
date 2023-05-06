23324c9fbb3c8d25a84f45b7f299efaeb63ebe78
jogi9928 <jogi9928@colorado.edu>
2023-05-05 23:53:19 -0600

    adding a die roll animation

---
+++ js/final.js
@@ -0,0 +1,32 @@
+// create variables and function to enact roll die animations
+var dieRoll = document.querySelector('.die-roll');
+var currentFace = '';
+var color = '';
+
+function rollDie()
+{
+    // generate random integer between 1 and 6 to see the value of the die role
+    var random = Math.floor(Math.random()*6) + 1;
+    console.log(random);
+
+    var newFace = 'face-' + random;
+    console.log(newFace);
+    if(newFace===currentFace)
+    {
+        var faceNum = '.face' + random;
+        var sameRoll = document.querySelector(faceNum);
+        sameRoll.addEventListener('animationend', function() {
+            sameRoll.classList.remove('changeColor');
+        });
+        sameRoll.classList.add('changeColor');
+    }
+    else
+    {
+        if(currentFace){dieRoll.classList.remove(currentFace);}
+        dieRoll.classList.add(newFace);
+        currentFace = newFace;
+    }
+}
+rollDie();
+// die rolls when user clicks it
+dieRoll.addEventListener('click', rollDie);
