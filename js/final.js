a2468b0c98ad5c6d2800b99ce79203096478ad28
jogi9928 <jogi9928@colorado.edu>
2023-05-07 17:57:18 -0600

    working on spaceship animation

--- js/final.js
+++ js/final.js
@@ -1,15 +1,27 @@
 // create variables and function to enact roll die animations
 var dieRoll = document.querySelector('.die-roll');
+var spaceCraft = document.getElementsByClassName('.spaceCraft');
 var currentFace = '';
 var color = '';
+let count = { position: 0 };
+var rollNumber = 0;
 
-function rollDie()
-{
+function moveCraft(rollNumber, count) { 
+    if(count.position===0)
+    {
+        count.position++;
+        rollNumber--;
+        spaceCraft.style.transform = 'translateX(7.8vw)'
+    } 
+}
+
+function rollDie(rollNumber, count) {
     // generate random integer between 1 and 6 to see the value of the die role
     var random = Math.floor(Math.random()*6) + 1;
     console.log(random);
 
     var newFace = 'face-' + random;
+    rollNumber = random;
     console.log(newFace);
     if(newFace===currentFace)
     {
@@ -19,14 +31,16 @@ function rollDie()
             sameRoll.classList.remove('changeColor');
         });
         sameRoll.classList.add('changeColor');
+        moveCraft(rollNumber, count);
     }
     else
     {
         if(currentFace){dieRoll.classList.remove(currentFace);}
         dieRoll.classList.add(newFace);
         currentFace = newFace;
+        moveCraft(rollNumber, count);
     }
 }
-rollDie();
+
 // die rolls when user clicks it
-dieRoll.addEventListener('click', rollDie);
+dieRoll.addEventListener('click', () => {rollDie(rollNumber, count);});
