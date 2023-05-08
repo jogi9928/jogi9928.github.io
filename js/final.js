e0246a24a901083a2df86f1f1af4993d5dc683bc
jogi9928 <jogi9928@colorado.edu>
2023-05-07 22:36:27 -0600

    spacecraft movement completed

--- js/final.js
+++ js/final.js
@@ -8,26 +8,62 @@ var rollNumber = 0;
 let currentLeft = { position: 0};
 let currentTop = { position: 0};
 
-function moveCraft() { 
+function rotateCraftUp() {
+    let promise = new Promise(resolve => {
+        setTimeout(() => resolve("done!"), 1000)
+    });
+        spaceCraft.style.transform = 'translateX(1.25vw) translateY(53.5vh) rotate(-45deg)';
+    return promise; 
+}
+function rotateCraftDown() {
+    let promise = new Promise(resolve => {
+        setTimeout(() => resolve("done!"), 1000)
+    });
+        spaceCraft.style.transform = 'translateX(1.25vw) translateY(53.5vh) rotate(135deg)';
+    return promise; 
+}
+function rotateCraftRight() {
+    let promise = new Promise(resolve => {
+        setTimeout(() => resolve("done!"), 1000)
+    });
+        spaceCraft.style.transform = 'translateX(1.25vw) translateY(53.5vh) rotate(45deg)';
+    return promise; 
+}
+async function moveCraft(rollNumber) { 
     // move ship right, up and down to follow gameboard path
-    if(count.count===0 || count.count>=5 && count.count<=7 || count.count===10 || count.count===11 || count.count===14 || count.count===15 || count.count===19)
-    {
-        currentLeft.position = currentLeft.position + 7.8;
-        spaceCraft.style.left = currentLeft.position + 'vw';
-        count.count++;
+    for(let i=0; i<rollNumber; i++){
+        if(count.count===0 || count.count>=5 && count.count<=7 || count.count===10 || count.count===11 || count.count===14 || count.count===15 || count.count===19)
+        {
+            if(count.count===5 || count.count===14 || count.count===10 || count.count===19)
+            { 
+                await rotateCraftRight();
+            }
+            currentLeft.position = currentLeft.position + 7.8;
+            spaceCraft.style.left = currentLeft.position + 'vw';
+            count.count++;
+        }
+        else if(count.count>=1 && count.count<=4 || count.count===12 || count.count===13)
+        {
+            if(count.count===1 || count.count===12)
+            {
+                await rotateCraftUp();
+            }
+            currentTop.position = currentTop.position - 13;
+            spaceCraft.style.top = currentTop.position + 'vh';
+            count.count++;
+        } 
+        else if(count.count===8 || count.count===9 || count.count>=16 && count.count<=18)
+        {
+            if(count.count===8 || count.count===16)
+            {
+                await rotateCraftDown();
+            }
+            currentTop.position = currentTop.position + 13;
+            spaceCraft.style.top = currentTop.position + 'vh';
+            count.count++;
+        }
+            
     }
-    else if(count.count>=1 && count.count<=4 || count.count===12 || count.count===13)
-    {
-        currentTop.position = currentTop.position - 13;
-        spaceCraft.style.top = currentTop.position + 'vh';
-        count.count++;
-    } 
-    else if(count.count===8 || count.count===9 || count.count>=16 && count.count<=18)
-    {
-        currentTop.position = currentTop.position + 13;
-        spaceCraft.style.top = currentTop.position + 'vh';
-        count.count++;
-    } 
 }
 
 function rollDie() {
@@ -53,10 +89,7 @@ function rollDie() {
         dieRoll.classList.add(newFace);
         currentFace = newFace; 
     }
-    for(let i=rollNumber; i>0; i--)
-    {
-        moveCraft();
-    }
+    moveCraft(rollNumber);
 }
 
 // die rolls when user clicks it
