103d2151f974a96fb0eb2575055e34983fb5f55d
jogi9928 <jogi9928@colorado.edu>
2023-05-07 20:50:51 -0600

    spacecraft animation WIP

--- js/final.js
+++ js/final.js
@@ -1,21 +1,36 @@
 // create variables and function to enact roll die animations
 var dieRoll = document.querySelector('.die-roll');
-var spaceCraft = document.getElementsByClassName('.spaceCraft');
+var spaceCraft = document.querySelector('#spaceCraft');
 var currentFace = '';
 var color = '';
-let count = { position: 0 };
+let count = { count: 0 };
 var rollNumber = 0;
+let currentLeft = { position: 0};
+let currentTop = { position: 0};
 
-function moveCraft(rollNumber, count) { 
-    if(count.position===0)
+function moveCraft() { 
+    // move ship right, up and down to follow gameboard path
+    if(count.count===0 || count.count>=5 && count.count<=7 || count.count===10 || count.count===11 || count.count===14 || count.count===15 || count.count===19)
     {
-        count.position++;
-        rollNumber--;
-        spaceCraft.style.transform = 'translateX(7.8vw)'
+        currentLeft.position = currentLeft.position + 7.8;
+        spaceCraft.style.left = currentLeft.position + 'vw';
+        count.count++;
+    }
+    else if(count.count>=1 && count.count<=4 || count.count===12 || count.count===13)
+    {
+        currentTop.position = currentTop.position - 13;
+        spaceCraft.style.top = currentTop.position + 'vh';
+        count.count++;
+    } 
+    else if(count.count===8 || count.count===9 || count.count>=16 && count.count<=18)
+    {
+        currentTop.position = currentTop.position + 13;
+        spaceCraft.style.top = currentTop.position + 'vh';
+        count.count++;
     } 
 }
 
-function rollDie(rollNumber, count) {
+function rollDie() {
     // generate random integer between 1 and 6 to see the value of the die role
     var random = Math.floor(Math.random()*6) + 1;
     console.log(random);
@@ -31,16 +46,18 @@ function rollDie(rollNumber, count) {
             sameRoll.classList.remove('changeColor');
         });
         sameRoll.classList.add('changeColor');
-        moveCraft(rollNumber, count);
     }
     else
     {
         if(currentFace){dieRoll.classList.remove(currentFace);}
         dieRoll.classList.add(newFace);
-        currentFace = newFace;
-        moveCraft(rollNumber, count);
+        currentFace = newFace; 
+    }
+    for(let i=rollNumber; i>0; i--)
+    {
+        moveCraft();
     }
 }
 
 // die rolls when user clicks it
-dieRoll.addEventListener('click', () => {rollDie(rollNumber, count);});
+dieRoll.addEventListener('click', rollDie);
