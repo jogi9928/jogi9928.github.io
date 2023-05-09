db214b4a5830131687e528d7a76c361d08c06674
jogi9928 <jogi9928@colorado.edu>
2023-05-08 20:22:08 -0600

    trivia aspect WIP

--- js/final.js
+++ js/final.js
@@ -7,6 +7,33 @@ let count = { count: 0 };
 var rollNumber = 0;
 let currentLeft = { position: 0};
 let currentTop = { position: 0};
+let turns = { rolls: 0 };
+
+
+// take trivia and multiple choice answers for API
+const endpoint = 'https://opentdb.com/api.php?amount=10&difficulty=easy&type=multiple';
+
+async function getQuote()
+{
+    try
+    {
+        const response = await fetch(endpoint);
+        if(!response.ok)
+        {
+            throw Error(response.statusText);
+        }
+        const json = await response.json();
+        console.log(json);
+    }
+    catch(err)
+    {
+        console.log(err);
+        alert('Failed to fetch new quote');
+    }
+}
+getQuote();
+
+// javascript for spaceship animation
 
 function rotateCraftUp() {
     let promise = new Promise(resolve => {
@@ -29,18 +56,50 @@ function rotateCraftRight() {
         spaceCraft.style.transform = 'translateX(1.25vw) translateY(53.5vh) rotate(45deg)';
     return promise; 
 }
+function goBack() {
+    var triviaGame = document.querySelector('.triviaGame');
+    triviaGame.style.display = 'none';
+    var gameBoard = document.querySelector('.gameBoard');
+    gameBoard.style.display = 'block'; 
+}
+function triviaG() {
+    var btn = document.querySelector('button');
+    btn.addEventListener('click', goBack);
+}
+
+
 async function moveCraft(rollNumber) { 
     // move ship right, up and down to follow gameboard path
     for(let i=0; i<rollNumber; i++){
         if(count.count===0 || count.count>=5 && count.count<=7 || count.count===10 || count.count===11 || count.count===14 || count.count===15 || count.count===19)
         {
-            if(count.count===5 || count.count===14 || count.count===10 || count.count===19)
+            if(count.count===10)
+            {
+                await rotateCraftRight();
+                currentLeft.position = currentLeft.position + 7.8;
+                spaceCraft.style.left = currentLeft.position + 'vw';
+                var gameBoard = document.querySelector('.gameBoard');
+                gameBoard.style.display = 'none';
+                var triviaGame = document.querySelector('.triviaGame');
+                triviaGame.style.display = 'block';
+                i=rollNumber;
+                triviaG();   
+                count.count++;             
+            }
+            else if(count.count===5 || count.count===14 || count.count===19)
             { 
                 await rotateCraftRight();
+                currentLeft.position = currentLeft.position + 7.8;
+                spaceCraft.style.left = currentLeft.position + 'vw';
+                count.count++;
+            } 
+            else
+            {
+                currentLeft.position = currentLeft.position + 7.8;
+                spaceCraft.style.left = currentLeft.position + 'vw';
+                count.count++;
             }
-            currentLeft.position = currentLeft.position + 7.8;
-            spaceCraft.style.left = currentLeft.position + 'vw';
-            count.count++;
+            
         }
         else if(count.count>=1 && count.count<=4 || count.count===12 || count.count===13)
         {
@@ -64,10 +123,32 @@ async function moveCraft(rollNumber) {
         }
             
     }
+    if(count.count===20)
+    {
+        var gameBoard = document.querySelector('.gameBoard');
+        gameBoard.style.display = 'none';
+        var triviaGame = document.querySelector('.triviaGame');
+        triviaGame.style.display = 'block';
+        i=rollNumber;
+        triviaG();   
+        count.count++; 
+    }
+}
+function pause1() {
+    let promise = new Promise(resolve => {
+        setTimeout(() => resolve("done!"), 1000)
+    });
+    return promise; 
 }
 
-function rollDie() {
+async function rollDie() {
     // generate random integer between 1 and 6 to see the value of the die role
+    dieRoll.removeEventListener('click', rollDie);
+
+    turns.rolls++;
+    var turnT = document.querySelector('#coins');
+    turnT.textContent = 'Turns: ' + turns.rolls;
+
     var random = Math.floor(Math.random()*6) + 1;
     console.log(random);
 
@@ -89,8 +170,12 @@ function rollDie() {
         dieRoll.classList.add(newFace);
         currentFace = newFace; 
     }
+    await pause1();
     moveCraft(rollNumber);
+    await pause1();
+    dieRoll.addEventListener('click', rollDie);
 }
-
-// die rolls when user clicks it
+// prevents user from spamming button
 dieRoll.addEventListener('click', rollDie);
+
+
